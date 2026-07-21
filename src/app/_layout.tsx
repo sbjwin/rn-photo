4b7ec76b7e5a8334dc5e9c57cea04a1a0e8f6603
sbjwin <sbjwin4271@gmail.com>
2026-07-21 18:46:31 +0900

    fix: remove deprecated ThemeProvider in layout - Expo Router(React Navigation v7) 업데이트로 인해 더 이상 지원되지 않는 `ThemeProvider` 컴포넌트 제거 - 앱

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,4 +1,3 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { useColorScheme } from 'react-native';
 
@@ -8,11 +7,10 @@ import AppTabs from '@/components/app-tabs';
 SplashScreen.preventAutoHideAsync();
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+    <>
       <AnimatedSplashOverlay />
       <AppTabs />
-    </ThemeProvider>
+    </>
   );
 }
