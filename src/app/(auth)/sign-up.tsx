ba5f40788b9cf5d26bec509ed436ebae8b53f854
sbjwin <sbjwin4271@gmail.com>
2026-08-05 15:06:55 +0900

    fix: 회원가입 화면(sign-up) 오타 및 레이아웃 오류 수정 - 오류 수정 및 copy 아이콘 변경

--- src/app/(auth)/sign-up.tsx
+++ src/app/(auth)/sign-up.tsx
@@ -1,15 +1,44 @@
+// 회원 가입 화면 구현
+
 import { ThemedText } from "@/components/themed-text";
 import { ThemedView } from "@/components/themed-view";
-import { StyleSheet } from "react-native";
+import { useRouter } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import { useEffect, useState } from "react";
+import { Image, StyleSheet, View } from "react-native";
+import { SafeAreaView, useSafeAreaInsets } from "react-native-safe-area-context";
 
 export default function SignUpScreen() {
+    const router = useRouter();
+    const { top } = useSafeAreaInsets();
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+    const [disable, setDisable] = useState(false);
+
+    useEffect(() => {
+        setDisable(!email || !password);
+    }, [email, password]);
+
     return (
-        <ThemedView style={styles.container}>
-            <ThemedText style={styles.title}>Sign Up</ThemedText>
-        </ThemedView>
+        <SafeAreaView style={{ flex: 1 }}>
+            <StatusBar style="light" />
+            <ThemedView style={[styles.container, { paddingTop: top }]}>
+                <View style={StyleSheet.absoluteFill}>
+                    <Image
+                        source={require('@/assets/images/cover.png')}
+                        style={{ width: '100%', height: '100%' }}
+                        resizeMode="cover"
+                    />
+                </View>
+                <ThemedText style={styles.title}>Sign Up</ThemedText>
+            </ThemedView>
+        </SafeAreaView>
     );
 }
 
+// 스타일 정의
 const styles = StyleSheet.create({
     container: {
         flex: 1,
